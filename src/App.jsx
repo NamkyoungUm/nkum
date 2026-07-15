d74d2cefe0648abf38341f50558d2d17b393ffbf
NamkyoungUm <53123805+NamkyoungUm@users.noreply.github.com>
2026-07-15 12:17:04 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -91,6 +91,7 @@ function App() {
         }>
           <div className = "App">
             <Routes>
+              <Route path="/nkum" element={<Home />}/>
               <Route path="/" element={<Home />}/>
               <Route path="/new" element={<New />}/>
               <Route path="/diary/:id" element={<Diary />}/>
